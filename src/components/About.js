b43efb62e3574f81c5aed1f240893df5df7cd3fc
sakshi <whitesky5672@gmail.com>
2021-09-27 15:10:38 +0530

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,14 @@ const About = () => {
     return (
         <div className="container">
             <p className="hr"></p>
-            <h1 id="aboutheading">About Me</h1>
+            <h1 id="aboutheading">About Us</h1>
             <div className="row">
                 <img src={summer} className="img-thumbnail rounded mx-auto d-block" alt="summer" />
             </div>   
             <div className="row center">
                 <p>
-                    I like to work, travel and try delicious foods from different parts of the world. I am always ready with my bags pack and take on another exciting adventure.
-                    You can also join me in my adventure. This blog is a great place for you to tell a story and let your users know a little more about you.
+                    This Blog is a great place for those who like to work, travel and try delicious foods from different parts of the world. We are always ready with our bags pack and take on another exciting trip.
+                    You can also join us in our adventure. This blog is a great place for you to tell your story and let the people know a little more about you.
                 </p>
             </div>
         </div>
